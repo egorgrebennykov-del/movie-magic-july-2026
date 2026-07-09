51264451c8fb3c0eca8212c172eba3a21aea790b
Yegor <egor.grebennykov@gmail.com>
2026-07-09 19:58:01 +0300

    Setup Handlebars

--- project/src/index.js
+++ project/src/index.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import { engine } from 'express-handlebars';
 
 const app = express();
 
+app.engine('hbs', engine({
+  defaultLayout: false
+}));
+app.set('view engine', 'hbs');
+app.set('views', './views');
+
 app.get('/', (req, res) => {
-    res.send('Hello World!!!');
+    res.render('home');
 });
 
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
