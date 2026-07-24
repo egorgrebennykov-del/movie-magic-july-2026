0112bd7891f67af2664214bb8e7a5a9912c6d29c
egorgrebennykov-del <egor.grebennykov@gmail.com>
2026-07-24 20:03:13 +0300

    Add Register to Main Navigation

--- project/controllers/authController.js
+++ project/controllers/authController.js
@@ -2,7 +2,7 @@ import { Router } from 'express'
 
 const authController = Router();
 
-authController.get('/registration', (req, res) => {
+authController.get('/register', (req, res) => {
     res.send('Register Page');
 });
 
