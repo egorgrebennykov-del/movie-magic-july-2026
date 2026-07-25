7010cea026757997bdfe30d72cf6764e002584e6
egorgrebennykov-del <egor.grebennykov@gmail.com>
2026-07-25 13:59:01 +0300

    Add Auth Middleware

---
+++ project/middlewares/authMiddleware.js
@@ -0,0 +1,3 @@
+export default function authMiddleware(req, res, next) {
+    console.log('Auth Middleware');
+}
--- project/src/index.js
+++ project/src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { engine } from 'express-handlebars';
 import routes from '../routes.js';
+import authMiddleware from '../middlewares/authMiddleware.js';
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.set('views', './views');
 app.use(express.static('./public'));
 
 app.use(express.urlencoded());
+app.use(authMiddleware);
 app.use(routes);
 
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
