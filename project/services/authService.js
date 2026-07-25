b549b069d227f5a86a974d06cf7e2c11dcb98fed
egorgrebennykov-del <egor.grebennykov@gmail.com>
2026-07-25 12:58:58 +0300

    Hash Password on User Create

--- project/services/authService.js
+++ project/services/authService.js
@@ -1,7 +1,14 @@
+import bcrypt from 'bcrypt';
 import userRepository from "../repositories/userRepository.js";
 
 export async function register(userData){
-    return await userRepository.create(userData);
+    const hashPassword = await bcrypt.hash(userData.password, 10);
+    const result = await userRepository.create({
+        ...userData,
+        password: hashPassword,
+    });
+
+    return result;
 }
 
 const authService = {
