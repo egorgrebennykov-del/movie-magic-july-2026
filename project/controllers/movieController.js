a75c1f94ada2676e7a11e48bc07ac851ce642401
egorgrebennykov-del <egor.grebennykov@gmail.com>
2026-07-26 15:54:30 +0300

    Modify Selected on Edit

--- project/controllers/movieController.js
+++ project/controllers/movieController.js
@@ -71,6 +71,24 @@ movieController.get('/:movieId/delete', isAuth, async (req, res) => {
     res.redirect('/');
 });
 
+function prepareCategoryViewData(movie) {
+    const categories = ['TV Show', 'Animation', 'Movie', 'Documentary', 'Short Film'];
+
+    const categoryOptions = categories.map(category => {
+        const value = category.toLowerCase().replaceAll(' ', '-');
+
+        const option = {
+            value,
+            label: category,
+            selected: movie.category === value
+        };
+
+        return option;
+    });
+
+    return categoryOptions;
+}
+
 movieController.get('/:movieId/edit', isAuth, async (req, res) => {
     const movieId = Number(req.params.movieId);
     const userId = req.user.id;
@@ -82,7 +100,9 @@ movieController.get('/:movieId/edit', isAuth, async (req, res) => {
         return res.status(404).send('Unauthorized');
     }
 
-    res.render('movies/edit', { movie, pageTitle: 'Edit' });
+    const categoryOptions = prepareCategoryViewData(movie);
+
+    res.render('movies/edit', { movie, categoryOptions, pageTitle: 'Edit' });
 });
 
 movieController.post('/:movieId/edit', async (req, res) => {
