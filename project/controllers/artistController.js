7d70c199a9cbee2f34c1c503b6a45e2d2b356ea3
egorgrebennykov-del <egor.grebennykov@gmail.com>
2026-07-29 15:49:52 +0300

    Show Multiple Errors on Create Artists

--- project/controllers/artistController.js
+++ project/controllers/artistController.js
@@ -1,20 +1,37 @@
+import * as z from 'zod';
 import { Router } from "express";
-import { artistService }  from "../services/artistService.js";
+import { artistService } from "../services/artistService.js";
 import { isAuth } from "../middlewares/authMiddleware.js";
+import { createArtistSchema } from "../schemas/artistSchema.js";
 
 const artistsController = Router();
 
 artistsController.get('/create', isAuth, (req, res) => {
-    res.render('artists/create');
+    res.render('artists/create', { pageTitle: 'Create' });
 });
 
 artistsController.post('/create', isAuth, async (req, res) => {
-    const artistData = req.body;
-    artistData.age = Number(artistData.age);
+    const newArtist = req.body;
 
-    await artistService.create(artistData);
+    try {
+        const artistData = createArtistSchema.parse(newArtist);
 
-    res.redirect('/');
+        await artistService.create(artistData);
+
+        res.redirect('/');
+    } catch (error) {
+        if (error instanceof z.ZodError) {
+            const errors = z.flattenError(error).fieldErrors;
+
+            return res.status(400).render('artists/create', {
+                artist: req.body,
+                errors,
+                pageTitle: 'Create'
+            });
+        }
+
+        throw error;
+    }
 });
 
 export default artistsController;
